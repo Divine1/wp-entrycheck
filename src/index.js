0516bfc4afe13b3a7a4258c528ecc043a7253b9c
System Administrator <root@divines-MacBook-Pro.local>
2019-03-29 08:55:12 +0530

    updated

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+console.log("in index.js")
+import home from './Home';
+import contact from './Contact';
+import sports from './Sports';
+import about from './About';
+
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+
+
+module.exports = {
+    // entry:
+    //     ()=>   new Promise((resolve) => resolve([
+    //         './src/Home.js', 
+    //         './src/Sports.js',
+    //         './src/About.js',
+    //         './src/Contact.js'])) 
+    entry : "./src/index.js"
+}
